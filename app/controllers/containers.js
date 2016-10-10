5ab45361946638e8f314a69e6d4233dd1957151b
alexpud <alexandre.maciele@gmail.com>
2016-10-10 11:30:02 -0300

    Managed to create a list container method containing the status of each container using promises

--- app/controllers/containers.js
+++ app/controllers/containers.js
@@ -12,13 +12,59 @@ router.get('/container',function ( req, res, next)
   res.render('container');
 });
 
-router.get('/list_containers', function( req, res, next)
+/*
+  First, it retrieves the containers list from the database then, it uses promises package to make
+  http requests to nginx for every container registration_ID, and creating a new JSON array with the
+  previous container list but with a new value, the status of the  container which is obtained as
+  a response from nginx.
+
+  I am using promises because it allows me to wait for all the requests inside the list promises to finish,
+  then it executes an action on Promises.all.
+ */
+router.get('/list_containers', function(req,res,next)
 {
   db.Container.findAll({raw:true}).then( function(container_list)
   {
-    //var _containers = container_list.get({plain: true});
-    console.log(container_list);
-    res.send(container_list);
+    var new_container_list = container_list;
+    var promises = [];
+
+    for( var i = 0; i< container_list.length; i++)
+    {
+      promises.push( new Promise(function( resolve,reject)
+      {
+        var options = {
+          host: 'localhost',
+          port: 8083,
+          path: '/'+container_list[i].registration_ID,
+          method: 'GET'
+        };
+
+        var req = http.request(options, function (res)
+        {
+          res.setEncoding('utf8');
+          res.on('data', function (chunk) {
+            console.log(chunk);
+            resolve
+            ({
+              data:chunk
+            });
+          });
+        });
+        req.end();
+      }));
+    }
+    Promise.all(promises).then(function(allData)
+    {
+      for(var i = 0; i < container_list.length;i++)
+      {
+        new_container_list[i].status = allData[i].data;
+      }
+
+      return new_container_list;
+    }).then(function(new_container_list)
+    {
+      res.send(new_container_list);
+    });
   });
 });
 
@@ -51,41 +97,6 @@ router.get('/container_status/:container', function( req, resi, next)
   }).end();
 
 });
-
-function getContainersStatus(container_list)
-{
-  var data ="";
-  for(var container in container_list) {
-    var options = {
-      host: '192.168.25.10',
-      port: 8083,
-      path: '/' + container.registration_ID,
-      method: 'GET'
-    };
-
-    var req = http.request(options, function (res) {
-      console.log('STATUS: ' + res.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-        data = chunk;
-        return data;
-      });
-    });
-    req.write("");
-    req.end();
-  }
-  return data;
-};
-
-
-router.post('/container_test', function( req, res, next)
-{
-  console.log(req.body);
-  res.render('containers');
-});
-
 
 /*
   It creates a container, both in the database and a container named after the user registration_ID.
@@ -132,3 +143,48 @@ router.post('/container', function (req, res, next)
   res.end();
 });
 
+function getContainersStatus(container_list)
+{
+
+  var new_container_list = container_list;
+  var promises = [];
+
+  for( var i = 0; i< container_list.length; i++)
+  {
+    promises.push( new Promise(function( resolve,reject)
+    {
+      var options = {
+        host: 'localhost',
+        port: 8083,
+        path: '/'+container_list[i].registration_id,
+        method: 'GET'
+      };
+
+      var req = http.request(options, function (res)
+      {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          console.log('BODY: ' + chunk);
+          console.log(chunk);
+          resolve
+          ({
+            data:chunk
+          });
+        });
+      });
+
+      req.end();
+    }));
+  }
+  Promise.all(promises).then(function(allData)
+  {
+    console.log(allData);
+    for( var i = 0; i < container_list; i ++)
+    {
+      new_container_list[i].status = allData[i].data;
+      console.log("lol");
+    }
+  });
+};
