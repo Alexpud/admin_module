c9797a2ec2fb4435431077ed2407dcf1584488e2
IsaiasSantana <isds.santana@gmail.com>
2016-11-04 15:46:45 -0300

    working on users.js, encrypting the password with library bcrypt

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,6 +5,31 @@ var express = require('express'),
   db = require('../models'),
   passport = require('passport');
 
+var bcrypt = require('bcrypt-nodejs');
+
+generatePassword= function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(9));
+}
+
+validatePassword = function(passwordForm,passwordDB){
+  return bcrypt.compareSync(passwordForm,passwordDB);
+}
+
+createUser = function(loginUser,passwordUser,tokenUser,isAdmin,req,res){
+   db.User.create(
+      {
+        login: loginUser,
+        password: generatePassword(passwordUser),
+        token: tokenUser,
+        admin: isAdmin
+      }).then(function(user){
+        res.json({'token':user.token});
+        res.status(200);
+        res.send();
+      });
+}
+
+
 module.exports = function (app) {
   app.use('/api', router);
 };
@@ -25,44 +50,35 @@ router.post("/users/:name",function(req,res)
 
 router.post('/users/:login/authenticate',function(req,res)
 {
-  db.User.findOrCreate
-  ({
-    where:
+  db.User.findAll({
+    limit: 1,
+    where: {
+      login: req.params.login
+    },
+    order: [ [ 'createdAt', 'DESC' ]]
+  }).then(function(user){
+    // user exist, return your token if password form match password data base
+    if(user[0] != null)
     {
-      login: req.params.login,
-      password: req.body.password
-    }
-  }).spread(function(user, created)
-  {
-    console.log(created)
-  	// new user created, so insert your token.
-  	if(created && user !== null)
-  	{
-	  	var tokenUser = jwt.sign({ user:req.body.login }, "ilovescotchyscotch", {
-	     // expiresIn : "5m" // expires in 24 hours
-	  });
-	  	//insert token on data base.
-  		db.User.update({ token: tokenUser }, //change login to field token on the table
-			{
-        where:
-        {
-          login: req.body.login,
-          password: req.body.password
-        }
-			}).then(function()
+      if(validatePassword(req.body.password,user[0].password))
       {
-				res.status(200);
-				res.send({"token": tokenUser});
-			}).catch(function (err)
-        {
-          res.status(409);
-          res.send({error: err.errors});
-        });
-  		// return the information including token as JSON
-  	}else if(user !== null)
-  	{ //retrieve token user
-  		res.status(200);
-  		res.send({ "token": user.token });
-  	}
-  });
+        res.status(200);
+        console.log(user[0].login);
+        res.json({'token': user[0].token})
+        res.send();        
+      }else
+      {
+          res.status(401);
+          res.send("Senha incorreta");
+      }
+
+    }
+    else //create new user and return your token
+    {
+      var tokenUser = jwt.sign({ user:req.body.login }, "Oursecretsecret", {
+        // expiresIn : "5m" // expires in 24 hours 
+      });
+      createUser(req.params.login,req.body.password,tokenUser,req.body.admin,req,res);     
+    }
+  }); 
 });
