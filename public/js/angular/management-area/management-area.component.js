ec171baa9ca6c8e9b46aa953626ec0a4160a2480
alexpud <alexandre.maciele@gmail.com>
2016-11-19 18:33:13 -0300

    Added a few skelleton methods

--- public/js/angular/management-area/management-area.component.js
+++ public/js/angular/management-area/management-area.component.js
@@ -8,6 +8,10 @@ angular
         function ManagementAreaController($scope)
         {
           var self = this;
+
+          /*
+            Gets a list of containers from backend
+           */
           self.getContainers = function()
           {
             $scope.get('api/containers').then(function (response)
@@ -17,10 +21,53 @@ angular
             });
           };
 
+          /*
+            Action that receives the id of the table containing the workspaces, and toggles the class hidden
+           */
           self.hideWorkspaces = function(id)
           {
             var button = angular.element( document.querySelector( '#'+id ) );
             button.toggleClass("hidden");
+          };
+
+          /*
+            Executes one of the actions for a given container
+           */
+          self.executeContainerAction = function(action)
+          {
+            switch(action)
+            {
+              case "start":
+                break;
+
+              case "stop":
+                break;
+
+              case "delete":
+                break;
+            }
+
+            self.getContainers();
+          };
+
+          /*
+            Executes a action for one of the given workspaces
+           */
+          self.executeWorkspaceAction = function (action)
+          {
+            switch(action)
+            {
+              case "start":
+                break;
+
+              case "stop":
+                break;
+
+              case "delete":
+                break;
+            }
+
+            self.getContainers();
           };
 
           self.getContainers();
