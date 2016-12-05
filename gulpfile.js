516a614a5e433c8d2dd1a9bd5d4ad8f87611124e
alexpud <alexandre.maciele@gmail.com>
2016-12-05 20:55:37 -0300

    Added browser sync task, for now only to index.html on angular app root folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,51 @@ var gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
   sass = require('gulp-sass'),
+  bs = require('browser-sync'),
+  watch = require('gulp-watch'),
   shell = require('gulp-shell');
 
 gulp.task('docker.sock', shell.task(
-  [
-    'sudo chmod 777 /var/run/docker.sock'
-  ],
+  ['sudo chmod 777 /var/run/docker.sock'],
   {
     verbose: true
   }
 ));
 
+gulp.task('watch', function()
+{
+  gulp.watch('public/js/angular/*.html',['build'])
+});
+
+gulp.task('build',function()
+{
+  bs.reload();
+  console.log("assdasd");
+});
+
 gulp.task('styles',function()
 {
   gulp.src('./public/js/angular/assets/css/style.scss')
-  .pipe(sass().on('error',sass.logError))
-  .pipe(gulp.dest('./public/js/angular/assets/css'));
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('./public/js/angular/assets/css'));
 });
+
 
 gulp.task('database', shell.task(
   [
     'docker start mysql_nginx'
   ]
 ));
+
+gulp.task('browser-sync',function() {
+  bs.init({
+    baseDir: "./public/js/angular/*.html",
+    proxy: 'localhost:3000/#/'
+  });
+
+  gulp.watch('public/js/angular/*.html').on('change',bs.reload);
+});
+
 
 gulp.task('develop', function () {
   livereload.listen();
